4b94d68ceffdab22d1f9a492b3d87e5249bc583c
ajay.kumar <ajay.kumar@softobiz.com>
2021-10-12 13:26:38 +0530

    set up db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeaEntity } from './idea/idea.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'SA',
+      password: 'India_1947',
+      database: 'ideasApi',
+      synchronize: true,
+      logging: true,
+      entities: [IdeaEntity],
+      extra: {
+        trustServerCertificate: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
